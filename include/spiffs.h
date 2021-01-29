79e4610fc612a24c2e1cbeea625714ff30499750
Justin T. Conroy <justin@conroy.in>
2021-01-29 03:04:19 -0600

    Lots of formatting.

--- include/spiffs.h
+++ include/spiffs.h
@@ -3,33 +3,44 @@
 
 namespace IDF
 {
-   class Spiffs
-   {
-   public:
-      Spiffs(const std::string basePath = "/spiffs",
-             const std::string partitionLabel = "",
-             const int MaxFilesOpen = 5,
-             const bool formatIfMountFailed = true);
+class Spiffs {
+public:
+  Spiffs(const std::string basePath       = "/spiffs",
+         const std::string partitionLabel = "",
+         const int MaxFilesOpen           = 5,
+         const bool formatIfMountFailed   = true);
 
-      void Register();
-      void Unregister();
-      void Format();
-      bool IsMounted();
+  void Register();
+  void Unregister();
+  void Format();
+  bool IsMounted();
 
-      size_t TotalSize();
-      size_t UsedSize();
+  size_t TotalSize();
+  size_t UsedSize();
 
-      std::string BasePath() { return _basePath; };
-      std::string PartitionLabel() { return _partitionLabel; };
-      int MaxFilesOpen() { return _maxFilesOpen; };
-      bool FormatIfMountFailed() { return _formatIfMountFailed; };
+  std::string BasePath()
+  {
+    return _basePath;
+  };
+  std::string PartitionLabel()
+  {
+    return _partitionLabel;
+  };
+  int MaxFilesOpen()
+  {
+    return _maxFilesOpen;
+  };
+  bool FormatIfMountFailed()
+  {
+    return _formatIfMountFailed;
+  };
 
-   private:
-      const std::string _basePath = "/spiffs";
-      const std::string _partitionLabel = "";
-      const int _maxFilesOpen = 5;
-      const bool _formatIfMountFailed = true;
-   }; // class Spiffs
+private:
+  const std::string _basePath       = "/spiffs";
+  const std::string _partitionLabel = "";
+  const int _maxFilesOpen           = 5;
+  const bool _formatIfMountFailed   = true;
+}; // class Spiffs
 } // namespace IDF
 #define COMPONENT_H
 #endif // COMPONENT_H
--- spiffs.cpp
+++ spiffs.cpp
@@ -1,28 +1,32 @@
+#include "spiffs.h"
+
 #include <sstream>
 #include <stdexcept>
 #include <esp_log.h>
 #include <esp_spiffs.h>
 
-#include "spiffs.h"
 using namespace IDF;
 using namespace std;
 
-static const char *TAG = "Spiffs++";
+static const char* TAG = "Spiffs++";
 
 Spiffs::Spiffs(const string basePath,
                const string partitionLabel,
                const int maxFilesOpen,
                const bool formatIfMountFailed)
-    : _basePath{basePath},
-      _partitionLabel{partitionLabel},
-      _maxFilesOpen{maxFilesOpen},
-      _formatIfMountFailed{formatIfMountFailed}
+  : _basePath{basePath},
+    _partitionLabel{partitionLabel},
+    _maxFilesOpen{maxFilesOpen},
+    _formatIfMountFailed{formatIfMountFailed}
 {
-  if (_maxFilesOpen <= 0)
-  {
-    ESP_LOGE(TAG, "parameter maxFilesOpen must be greater than zero. got: %d", _maxFilesOpen);
+  if (_maxFilesOpen <= 0) {
+    ESP_LOGE(TAG,
+             "parameter maxFilesOpen must be greater than zero. got: %d",
+             _maxFilesOpen);
 #ifdef CONFIG_COMPILER_CXX_EXCEPTIONS
     throw std::invalid_argument("maxFilesOpen");
+#if 1
+#endif
 #else
     abort();
 #endif
@@ -34,25 +38,25 @@ void Spiffs::Register()
   ESP_LOGI(TAG, "Initializing SPIFFS");
 
   esp_vfs_spiffs_conf_t conf;
-  conf.base_path = _basePath.c_str();
-  conf.partition_label = _partitionLabel.empty() ? NULL : _partitionLabel.c_str();
-  conf.max_files = _maxFilesOpen;
+  conf.base_path              = _basePath.c_str();
+  conf.max_files              = _maxFilesOpen;
   conf.format_if_mount_failed = _formatIfMountFailed;
+  conf.partition_label        = _partitionLabel.empty() ? NULL
+                                                 : _partitionLabel.c_str();
 
   esp_err_t ret = esp_vfs_spiffs_register(&conf);
 
-  if (ret != ESP_OK)
-  {
-    if (ret == ESP_FAIL)
-    {
-      ESP_LOGE(TAG, "Failed to mount or format filesystem. (%s)", esp_err_to_name(ret));
+  if (ret != ESP_OK) {
+    if (ret == ESP_FAIL) {
+      ESP_LOGE(TAG,
+               "Failed to mount or format filesystem. (%s)",
+               esp_err_to_name(ret));
     }
-    else if (ret == ESP_ERR_NOT_FOUND)
-    {
-      ESP_LOGE(TAG, "Failed to find SPIFFS partition (%s)", esp_err_to_name(ret));
+    else if (ret == ESP_ERR_NOT_FOUND) {
+      ESP_LOGE(
+        TAG, "Failed to find SPIFFS partition (%s)", esp_err_to_name(ret));
     }
-    else
-    {
+    else {
       ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
     }
 #ifdef CONFIG_COMPILER_CXX_EXCEPTIONS
@@ -67,17 +71,18 @@ void Spiffs::Register()
 
   size_t total = 0, used = 0;
   ret = esp_spiffs_info(conf.partition_label, &total, &used);
-  if (ret != ESP_OK)
-  {
-    ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+  if (ret != ESP_OK) {
+    ESP_LOGE(TAG,
+             "Failed to get SPIFFS partition information (%s)",
+             esp_err_to_name(ret));
   }
-  else
-  {
+  else {
     ESP_LOGI(TAG, "Partition size: total: %d, used %d", total, used);
   }
 }
 
 void Spiffs::Unregister()
 {
-  esp_vfs_spiffs_unregister(_partitionLabel.empty() ? NULL : _partitionLabel.c_str());
+  esp_vfs_spiffs_unregister(_partitionLabel.empty() ? NULL
+                                                    : _partitionLabel.c_str());
 }
